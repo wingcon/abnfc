47f9de0f21a66f2cd0fe9ed01e931127d5d99e3a
Joseph Wayne Norton <norton@alum.mit.edu>
2010-09-25 02:08:47 +0900

    change request - add new rfc4234 parser that supports case-sensitive strings

--- src/abnfc.erl
+++ src/abnfc.erl
@@ -1,15 +1,15 @@
 %%%-------------------------------------------------------------------
 %%% @copyright 2009 Anders Nygren
-%%% @version  {@vsn} 
+%%% @version  {@vsn}
 %%% @author Anders Nygren <anders.nygren@gmail.com>
-%%% @doc 
-%%% @end 
+%%% @doc
+%%% @end
 %%%-------------------------------------------------------------------
 -module(abnfc).
 
 %% API
 -export([file/1, file/2,
-	 parse/1, parse/2]).
+     parse/1, parse/2]).
 
 -export([erlangcode/0]).
 
@@ -35,21 +35,21 @@ file(File) ->
 %%--------------------------------------------------------------------
 file(File, Opts) when is_list(Opts) ->
     case read_file(File) of
-	{ok, Name, Text} ->
-	    POpts = [],
-	    GenOpts = gen_opts(Name, Opts),
-	    COpts = compiler_opts(Opts),
-	    case parse(Text, POpts) of
-		{ok, AST, _Rest} ->
-		    AST1 = abnfc_ast:ast_to_int_form(AST),
-		    {ok, Code} = abnfc_gen:generate(AST1, GenOpts),
-		    {ok, GenFile} = write_file(Code, GenOpts),
-		    compile_file(GenFile, COpts);
-		Error ->
-		    Error
-	    end;
-	Error ->
-	    Error
+    {ok, Name, Text} ->
+        POpts = [],
+        GenOpts = gen_opts(Name, Opts),
+        COpts = compiler_opts(Opts),
+        case parse(Text, POpts) of
+        {ok, AST, _Rest} ->
+            AST1 = abnfc_ast:ast_to_int_form(AST),
+            {ok, Code} = abnfc_gen:generate(AST1, GenOpts),
+            {ok, GenFile} = write_file(Code, GenOpts),
+            compile_file(GenFile, COpts);
+        Error ->
+            Error
+        end;
+    Error ->
+        Error
     end.
 
 %%--------------------------------------------------------------------
@@ -70,12 +70,13 @@ parse(Bin) ->
 parse(Bin, Opts) when is_binary(Bin) ->
     parse(binary_to_list(Bin), Opts);
 
-parse(String, _Opts) when is_list(String) ->
-    case catch abnfc_rfc4234:rulelist_dec(String) of
-	{ok, _Rulelist, []} =Result ->
-	    Result;
-	_Error ->
-	    io:format("abnfc: failed~n",[])
+parse(String, Opts) when is_list(String) ->
+    Parser = proplists:get_value(parser, parse_opts(Opts)),
+    case catch Parser:rulelist_dec(String) of
+    {ok, _Rulelist, []} =Result ->
+        Result;
+    _Error ->
+        io:format("abnfc: failed~n",[])
     end.
 
 %%--------------------------------------------------------------------
@@ -85,36 +86,36 @@ parse(String, _Opts) when is_list(String) ->
 %%--------------------------------------------------------------------
 erlangcode() ->
     fun (T) ->
-	    scan(T)
+        scan(T)
     end.
 
 scan(Input) ->
     case erl_scan:tokens([], Input, 1) of
-	{done, {ok, Toks, _EndLine}, Extra} ->
-	    Code = toks_to_list(Toks),
-	    {ok, Code, Extra};
-	{more, _Cont} ->
-	    throw(end_of_input)
+    {done, {ok, Toks, _EndLine}, Extra} ->
+        Code = toks_to_list(Toks),
+        {ok, Code, Extra};
+    {more, _Cont} ->
+        throw(end_of_input)
     end.
 
 %%--------------------------------------------------------------------
-%% @private 
+%% @private
 %% @spec (Tokens) -> list()
 %% @doc Convert tokens returned by erl_scan to a string again.
 %% @end
 %%--------------------------------------------------------------------
 toks_to_list(Tokens) ->
     lists:foldl(fun({atom,L,Name},{Line, Acc}) ->
-			{L,["'",Name,"'",sep(L,Line)|Acc]};
-		   ({string,L,Name},{Line, Acc}) ->
-			{L,["\"",Name,"\"",sep(L,Line)|Acc]};
-		   ({_Type,L,Name},{Line, Acc}) ->
-			{L,[Name,sep(L,Line)|Acc]};
-		   ({dot,_L},{_Line,Acc}) ->
-			lists:concat(lists:reverse(Acc));
-		   ({Reserved, L},{Line,Acc}) ->
-			{L,[Reserved,sep(L,Line)|Acc]}
-		end, {1,[]}, Tokens).
+            {L,["'",Name,"'",sep(L,Line)|Acc]};
+           ({string,L,Name},{Line, Acc}) ->
+            {L,["\"",Name,"\"",sep(L,Line)|Acc]};
+           ({_Type,L,Name},{Line, Acc}) ->
+            {L,[Name,sep(L,Line)|Acc]};
+           ({dot,_L},{_Line,Acc}) ->
+            lists:concat(lists:reverse(Acc));
+           ({Reserved, L},{Line,Acc}) ->
+            {L,[Reserved,sep(L,Line)|Acc]}
+        end, {1,[]}, Tokens).
 
 sep(L,L) ->
     " ";
@@ -126,17 +127,21 @@ sep(_,_) ->
 %%====================================================================
 read_file(File) ->
     case string:tokens(filename:basename(File), ".") of
-	[Name,"set","abnf"] ->
-	    {ok, Files} = file:consult(File),
-	    {ok, Name, lists:flatten([read_file1(F) || F <- Files])};
-	[Name, "abnf"] ->
-	    {ok, Name, read_file1(File)}
+    [Name,"set","abnf"] ->
+        {ok, Files} = file:consult(File),
+        {ok, Name, lists:flatten([read_file1(F) || F <- Files])};
+    [Name, "abnf"] ->
+        {ok, Name, read_file1(File)}
     end.
 
 read_file1(File) ->
     {ok, Bin} = file:read_file(File),
     binary_to_list(Bin).
-    
+
+parse_opts(Opts) ->
+    Parser = proplists:get_value(parser, Opts, abnfc_rfc4234),
+    [{parser,Parser}].
+
 gen_opts(Name, Opts) ->
     Mod = proplists:get_value(mod, Opts, Name),
     [{mod,Mod}].
@@ -145,10 +150,10 @@ compiler_opts(Opts) ->
     OutDir = proplists:get_value(o, Opts, "./"),
     IncludeDirs = [{i,Dir}||Dir <- proplists:get_all_values(i, Opts)],
     [{outdir,OutDir}|IncludeDirs].
-    
+
 write_file(Code, Opts) ->
     Name = filename:join(proplists:get_value(o, Opts, "."),
-			 proplists:get_value(mod, Opts))++".erl",
+             proplists:get_value(mod, Opts))++".erl",
     io:format("abnfc: writing to ~p~n",[Name]),
     file:write_file(Name, Code),
     erl_tidy:file(Name,[{backups,false}]),
---
+++ src/rfc4234ext_core.hrl
@@ -0,0 +1,11 @@
+%% Callback functions for rfc 4234 core extension rules.
+
+'LF_cb'(_) ->'LF'.
+'CR_cb'(_) -> 'CR'.
+'CRLF_cb'(_) -> 'CRLF'.
+'SQUOTE_cb'(_) -> 'SQUOTE'.
+'DQUOTE_cb'(_) -> 'DQUOTE'.
+'HTAB_cb'(_) -> 'HTAB'.
+'LWSP_cb'(_) -> 'LWSP'.
+'SP_cb'(_) -> 'SP'.
+'WSP_cb'(_) -> 'WSP'.
